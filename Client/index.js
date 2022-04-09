ed90a10663daca26c205df4504a12d152067cb35
S M Iftekhar Uddin <Uddin.SMI@gmail.com>
2022-04-09 13:42:14 -0400

    adding Components folder and minor update to index.js file

--- Client/index.js
+++ Client/index.js
@@ -2,12 +2,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import store from './store.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
     <React.StrictMode>
         <Provider store={store}>
