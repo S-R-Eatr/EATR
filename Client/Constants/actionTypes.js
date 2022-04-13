667cd25197e785de3083e7803e46f28485409278
Julian Kang <julianswkang@gmail.com>
2022-04-12 21:21:18 -0400

    log in page rendering, switching to signup ok

--- Client/Constants/actionTypes.js
+++ Client/Constants/actionTypes.js
@@ -3,3 +3,4 @@ export const ADD_TO_FAVS = 'ADD_TO_FAVS';
 export const GET_FAVS = 'GET_FAVS';
 export const GET_NEXT = 'GET_NEXT';
 export const SET_SCENE = 'SET_SCENE';
+export const GET_ISNEWUSER = 'GET_ISNEWUSER';
